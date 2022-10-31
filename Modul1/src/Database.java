a932b8db1bfeb64d5473837401bc7cefabf3c02c
nurulfitrialyh <90302125+nurulfitrialyh@users.noreply.github.com>
2022-10-31 08:37:55 +0700

    Add files via upload

---
+++ Modul1/src/Database.java
@@ -0,0 +1,9 @@
+public class Database {
+
+        public static void main(String[] args) {
+            System.out.println("Selamat Datang di Restoran EAD");
+            System.out.println("Silahkan Register Terlebih Dahulu");
+            System.out.println("Nama = Nurul Fitri Aliyah");
+            
+    }
+}
---
+++ Modul1/src/Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+    public static void main(String[] args) {
+        System.out.println("Menu : ");
+        System.out.println("1. Nasi Goreng : 10.000");
+        System.out.println("2. ");
+
+    
+
+    }
+    
+    
+    
+}
